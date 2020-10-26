574b19966b296c5adf8afc43a80b45ddb2798208
Joaquin <carbonell.joaquin27@gmail.com>
2020-10-25 18:58:45 -0600

    clone

---
+++ Constraint.js
@@ -0,0 +1,17 @@
+class ConstraintClass {
+    constructor(bodyA,bodyB) {
+        var options = {
+            bodyA: bodyA,
+            bodyB: bodyB,
+            stiffness: 0.1,
+            length: 1
+        }
+        this.chain = Constraint.create(options);
+        World.add(world,this.chain);
+    }
+    display(){
+    var pointA = this.chain.bodyA.position;
+    var pointB = this.chain.bodyB.position;
+    line(pointA.x,pointA.y,pointB.x,pointB.y);
+    }
+}
--- sketch.js
+++ sketch.js
@@ -1,11 +1,14 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
 var test;
+var log6;
+var chain;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -34,9 +37,11 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
+    log6 = new Log(100,100,150,PI/2);
 
     bird = new Bird(100,100);
 
+    chain = new ConstraintClass(bird.body,log6.body);
 }
 
 function draw(){
@@ -59,7 +64,9 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+    log6.display();
 
+    chain.display();
     bird.display();
     platform.display();
 }
